3f381fe6809b5b03b3a8e86acf4ff2a876f2f325
Encrypter <witness.hbsct@gmail.com>
2018-12-13 13:00:53 +0200

    Ecommerce Logic Implementation Vue

--- public/script.js
+++ public/script.js
@@ -1,1 +1,43 @@
-console.log("It works!");
+new Vue({
+	el: "#app",
+	data: {
+		total: 0.00,
+		cart:[],
+		products: [{
+				title: "Product One",
+				id: 1,
+				price: 10
+			},
+			{
+				title: "Product Two",
+				id: 2,
+				price: 11
+			},
+			{
+				title: "Product Three",
+				id: 3,
+				price: 5
+			}
+		]
+	},
+	methods: {
+		addToCart: function(product) {
+			this.total += product.price;
+			var found = false;
+			for (var i = 0; i < this.cart.length; i++) {
+				if (this.cart[i].id === product.id) {
+					found = true;
+					this.cart[i].qty++;
+				}
+			}
+			if (!found) {
+				this.cart.push({
+					id: product.id,
+					title: product.title,
+					price: product.price,
+					qty: 1
+				});
+			}
+		}
+	}
+});
